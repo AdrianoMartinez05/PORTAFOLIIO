b9a8085b3f7fc6e2bee42332414d60f4231e3c4d
AdrianoMartinez <131898590+AdrianoMartinez05@users.noreply.github.com>
2025-02-05 21:10:15 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 (function (document) {
-    // Seleccionar las tarjetas y el elemento con la clase 'blue'
-    const tarjetas = document.querySelectorAll('.tarjeta-servicio.ttt.invisible');
-    const blue = document.querySelector('.blue');
+  const markers = [...document.querySelectorAll('mark')];
   
-    // Validar si existe el elemento 'blue' para evitar errores
-
-  
-    // Configurar IntersectionObserver para observar la clase 'blue'
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            // Si 'blue' es visible, cargar las tarjetas
-            tarjetas.forEach((tarjeta) => {
-              tarjeta.classList.add('animated', 'fadeInLeft');
-              tarjeta.classList.remove('invisible');
-            });
-  
-            // Dejar de observar 'blue' después de activar la animación
-            observer.unobserve(entry.target);
-          }
-        });
-      },
-      {
-        threshold: 0.8, // Umbral del 80% de visibilidad
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.style.animationPlayState = 'running';
+        observer.unobserve(entry.target);
       }
-    );
+    });
+  }, {
+    threshold: 0.8
+  });
   
-    // Observar el elemento con la clase 'blue'
-    observer.observe(blue);
-  })(document);
-  
+  markers.forEach(mark => {
+    observer.observe(mark);
+  });
+})(document);
